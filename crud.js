e8523b01a75da83666199516442f5c72e88ebb85
krobingit <91210322+krobingit@users.noreply.github.com>
2021-10-19 14:19:30 +0530

    Update crud.js

--- crud.js
+++ crud.js
@@ -13,7 +13,7 @@ document.body.innerHTML = `
              <input class="user-add-pic" Placeholder="Enter your pic url">
              </div >
              <div>
-             <button class="btn" onclick="addUser()">ADD</button>
+             <button class="btn" onclick="addUser()">CREATE</button>
              </div>
         </div>
       <div class="container">
